388efe4cf9275005fe03f0fcf133729dee943858
kavivpk <kaviyarasu4980@gmail.com>
2026-08-15 20:53:20 +0530

    Use environment variables for database connection

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,4 @@
 
-# import mysql.connector
-
-# def get_db_connection():
-#     conn = mysql.connector.connect(
-#         host="localhost",
-#         user="root",
-#         password="Kavi@2006",
-#         database="web_analyzes"
-#     )
 
 import os
 import mysql.connector
